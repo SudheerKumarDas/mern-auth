d35e802bfac6ae248fef3800583e8403334a53b4
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-06-11 13:02:17 +0545

    user model created ie schema for users

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import connectDB from "./src/config/db.js";
+import authRoutes from "./src/routes/auth.route.js"
 
 dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+
+app.use("/api/auth",authRoutes);
 
 const PORT = process.env.PORT || 3000;
 
---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const register = async (req,res) => {
+    res.send("User register");
+}
+
+export const login = async (req,res) => {
+    res.send("User login");
+}
+
+export const logout = async (req,res) => {
+    res.send("user logout");
+}
